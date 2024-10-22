a7407eb27a4061fa97a9642289e2bd8928796f4f
Luigi Pocay <luigiapocay92@gmail.com>
2024-10-22 20:47:40 -0300

    feat(EliminarTODO): Validado que el indice sea valido al momento de eliminar

--- src/eliminar-todo-comando.test.ts
+++ src/eliminar-todo-comando.test.ts
@@ -5,7 +5,7 @@ import { TODO, TODOEstados } from './types';
 
 describe('Eliminar TODOs', () => {
 
-  test('Eliminar TODO de archivo que contiene un TODO', () => {
+  test('Eliminar TODO de lista de TODO', () => {
     const todos: TODO[] = [
       {
         titulo: "Primer TODO",
@@ -19,4 +19,22 @@ describe('Eliminar TODOs', () => {
     expect(todosModificado.length).toEqual(0);
   });
 
+  test('Eliminar TODO de lista con indice invalido', () => {
+    const todos: TODO[] = [
+      {
+        titulo: "Primer TODO",
+        estado: TODOEstados.SinEmpezar,
+      }
+    ];
+    const comandoEliminar = new EliminarTODOs();
+    let indice_eliminar = 1;
+
+    let funcionTest = () => comandoEliminar.ejecutar(todos, indice_eliminar);
+    expect(funcionTest).toThrowError("Indice invalido");
+
+    indice_eliminar = -1;
+
+    funcionTest = () => comandoEliminar.ejecutar(todos, indice_eliminar);
+    expect(funcionTest).toThrowError("Indice invalido");
+  });
 });
--- src/eliminar-todo-comando.ts
+++ src/eliminar-todo-comando.ts
@@ -3,6 +3,10 @@ import { TODO } from './types';
 
 export class EliminarTODOs implements Comando {
   ejecutar(todos: TODO[], indice_eliminar: number): TODO[] {
+    const totalTODOs = todos.length;
+    if (indice_eliminar < 0 || indice_eliminar > totalTODOs - 1) {
+      throw new Error("Indice invalido");
+    }
     const todosModificados = todos.filter((_, i) => i !== indice_eliminar);
     return todosModificados;
   }
